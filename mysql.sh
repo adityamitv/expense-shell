8503a301b5911f42350ac5b46ea4eecab6500cae
adityamitv <shushma.valmiki@gmail.com>
2024-04-22 16:07:47 +0530

    hello-script

--- mysql.sh
+++ mysql.sh
@@ -8,6 +8,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+echo "please enter DB password:"
+read -s mysql_root_password
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -40,10 +42,10 @@ VALIDATE $? "starting MYSQL server"
 # VALIDATE $? "setting up root password"
 
 # below code will be useful for idempotent nature
-mysql -h db.devopshub.shop -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.devopshub.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if  [ $? -ne 0 ]
 then 
-   mysql_secure_installation --set-root-pass ExpenseApp@1 &>>?LOGFILE
+   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>?LOGFILE
    VALIDATE $? "MYSQL Root password setup"
 else 
     echo -e "MYSQL Root Password is already setup...$Y SKIPPING $N"
