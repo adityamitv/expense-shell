e167651e056be4b7a835befd47ae07eb3b27fca9
adityamitv <shushma.valmiki@gmail.com>
2024-04-22 18:19:30 +0530

    hello-script

--- backend.sh
+++ backend.sh
@@ -49,13 +49,14 @@ else
      echo -e "Expenses user alredy creaated...$Y SKIPPING $N" 
 fi
 
-mkdir -p /app  &>>$LOGFILE
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip  &>>$LOGFILE
 VALIDATE $? "Downloading backend code"
 
 cd /app  
+rm -rf /app/*
 unzip /tmp/backend.zip  &>>$LOGFILE
 VALIDATE $? "Extracted backen code"
 
