96f3a16ed55ff53e675b87d528b551faadf02251
adityamitv <shushma.valmiki@gmail.com>
2024-04-22 19:22:58 +0530

    hell0

--- mysql.sh
+++ mysql.sh
@@ -9,7 +9,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 echo "please enter DB password:"
-read -s mysql_root_password
+read  mysql_root_password
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -23,7 +23,7 @@ else
 
 if [ $USERID -ne 0 ]
 then
-   echo "please ru this script with root access."
+   echo "please run this script with root access."
    exit 1 # manually exit if error comes.
 else
      echo "you are super user."
@@ -33,16 +33,16 @@ dnf install mysql-server -y &>>$LOGFILE
 VALIDATE $? "Installing MYSQL server"
 
 systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "enabling MYSQL server"
+VALIDATE $? "Enabling MYSQL server"
 
 systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "starting MYSQL server"
+VALIDATE $? "Starting MYSQL server"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 # VALIDATE $? "setting up root password"
 
 # below code will be useful for idempotent nature
-mysql -h db.devopshub.shop -uroot -p${mysql_root_password} -e'show databases;' &>>$LOGFILE
+mysql -h db.devopshub.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then 
    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
